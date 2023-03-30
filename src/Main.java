097cb5c49023a0d928e7260b0be3f79ea3b6f051
alexdenzero <alexdenzero@yandex.ru>
2023-03-31 01:23:56 +0300

    object

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        Autor autor = new Autor ();
-        Book book = new Book("Война и мир",autor,1865);
+        Autor nameAutor = new Autor ("Лев Толстой");
+        Book book = new Book("Война и мир",nameAutor,1865);
         System.out.println("Book.name = " + book.getName());
-        System.out.println("Book.nameAutor = " + book.getAutor());
+        System.out.println("Book.nameAutor = " + nameAutor.getAutor());
         System.out.println("Book.age = " + book.getAge());
         book.setAge(1869);
         System.out.println("Book.age = " + book.getAge());
 
 
-        Autor autor2 = new Autor ();
-        Book book1 = new Book("Мир и война",autor2, 1866);
+        Autor nameAutor1 = new Autor ("Толстой Лев");
+        Book book1 = new Book("Мир и война",nameAutor, 1866);
         System.out.println("Book.name = " + book1.getName());
-        System.out.println("Book.nameAutor = " + book1.getAutor());
+        System.out.println("Book.nameAutor = " + nameAutor1.getAutor());
         System.out.println("Book.age = " + book1.getAge());
         book1.setAge(1868);
         System.out.println("Book.age = " + book1.getAge());
