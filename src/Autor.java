fa28b6e7f744442185efecc89eba549f8be19596
alexdenzero <alexdenzero@yandex.ru>
2023-03-30 17:45:39 +0300

    object

--- src/Autor.java
+++ src/Autor.java
@@ -1,10 +1,2 @@
 public class Autor {
-    private String nameAutor;
-
-    public Autor(String nameAutor) {
-        this.nameAutor = nameAutor;
-    }
-    public String getNameAutor() {
-        return this.nameAutor;
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Book Book = new Book("Война и мир", 1865);
-        Autor Autor = new Autor("Лев Толстой");
-        System.out.println("Book.name = " + Book.getName());
-        System.out.println("Book.nameAutor = " + Autor.getNameAutor());
-        System.out.println("Book.age = " + Book.getAge());
-        Book.setAge(1869);
-        System.out.println("Book.age = " + Book.getAge());
+
+        Autor autor = new Autor ();
+        Book book = new Book("Война и мир",autor,1865);
+        System.out.println("Book.name = " + book.getName());
+        System.out.println("Book.nameAutor = " + book.getAutor());
+        System.out.println("Book.age = " + book.getAge());
+        book.setAge(1869);
+        System.out.println("Book.age = " + book.getAge());
 
 
-        Book Book1 = new Book("Мир и война", 1866);
-        Autor Autor1 = new Autor("Толстой лев");
-        System.out.println("Book.name = " + Book1.getName());
-        System.out.println("Book.nameAutor = " + Autor1.getNameAutor());
-        System.out.println("Book.age = " + Book1.getAge());
-        Book.setAge(1868);
-        System.out.println("Book.age = " + Book1.getAge());
+        Autor autor2 = new Autor ();
+        Book book1 = new Book("Мир и война",autor2, 1866);
+        System.out.println("Book.name = " + book1.getName());
+        System.out.println("Book.nameAutor = " + book1.getAutor());
+        System.out.println("Book.age = " + book1.getAge());
+        book1.setAge(1868);
+        System.out.println("Book.age = " + book1.getAge());
     }
 }
